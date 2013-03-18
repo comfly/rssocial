878079011c9184d49babb66f08afd17f8f108ba0
Dmitry Zakharov <dmitry.zakharov@e-legion.com>
2013-03-18 18:34:25 +0400

    Refactor Feeder

--- vendor/feeder/feeder.rb
+++ vendor/feeder/feeder.rb
@@ -1,7 +1,21 @@
 require 'feedzirra'
 
-module Feeder
-  def get_entries_for_feed(feed)
+class Feeder
+  
+  def self.get_new_remote_entries_for_local_feed(local_feed)
+    all_entries_from_feed = self.get_entries_for_feed(local_feed)
+    self.filter_entries(local_feed.entries, all_entries_from_feed)
+  end
+  
+  def self.store_new_remote_entries_in_local_feed(remote_entries, local_feed)
+    remote_entries_array = remote_entries.to_a
+    remote_entries_array.each { |remote_entry| local_feed.entries << remote_entry }
+    local_feed.save
+  end
+  
+private
+
+  def self.get_entries_for_feed(feed)
     Feedzirra::Feed.fetch_and_parse(feed.feed_url).entires.inject(Array.new) do | entries, feedzirra_entry |
       entry = Entry.new
       entry.feed_id = feed.id
@@ -17,7 +31,10 @@ module Feeder
     end
   end
 
-  def get_feedzirra_feed_for_feed(feed)
-
+  def self.filter_entries(local_entries, remote_entries)
+    local_feed_items = local_entries.to_a.map { |local_entry| { title: local_entry.title, url: local_entry.url, summary: local_entry.summary } }
+    remote_entries.to_a.select do |remote_entry|
+      local_feed_items.index { |local_feed_item| (local_feed_item.title == remote_entry.title) && (local_feed_item.url == remote_entry.url) && (local_feed_item.summary == local_entry.summary) }.nil?
+    end
   end
 end
