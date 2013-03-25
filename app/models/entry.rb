20581947c993fd5ec187a590575931d5e212b9d7
Dmitry Zakharov <dmitry.zakharov@e-legion.com>
2013-03-25 16:22:29 +0400

    Modernize Order statement for Entry

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -2,7 +2,7 @@ class Entry < ActiveRecord::Base
   validates :url, presence: true
   validates :published, presence: true
 
-  scope :recent, -> (number_of_items) { order('created_at DESC').limit(number_of_items) }
+  scope :recent, -> (number_of_items = 10) { order(:created_at => :desc).limit(number_of_items) }
 
   belongs_to :feed, :inverse_of => :entries
   has_many :entry_statuses, :inverse_of => :entry, validate: true, :dependent => :destroy 
